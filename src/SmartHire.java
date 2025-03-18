ee1b06e2c4ac80ab9f4b540b03dd472d9b2609cc
Robert Sneddon <rharok225@hotmail.co.uk>
2025-03-18 09:41:05 +0000

    - Created template for GUI - Added Java doc to SmartHire -Added Main to SmartHire

---
+++ src/SmartHire.java
@@ -0,0 +1,10 @@
+/**
+ * SmartHire IQ Team Project
+ * Team: Roksana Blazejczyk, Marek Cudak, Robert Sneddon, Daniel Virlan
+ */
+public class SmartHire {
+
+    public static void main(String[] args) {
+
+    }
+}
